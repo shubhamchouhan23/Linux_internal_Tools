9adceccf13b637fd0450d459e927770b1f2ea98e
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-03-11 22:43:55 +0530

    Create Que_5.c

---
+++ Assignment-1/Que_5.c
@@ -0,0 +1,36 @@
+/***
+Program Returning “ls -l ” kind of structure of information from an existing file or
+opened file.
+***/
+
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdlib.h>
+
+char init[] = "\0";
+
+int main(void)
+{
+     int fd = creat("file.txt", 777);			//create file
+     
+     if(fd < 0)
+     {
+            perror("Creation error");
+	    exit(1);
+     }
+     
+     if(lseek(fd,4094,SEEK_SET) < 0)			//4KB free space creation	--by shifting fd to 4094
+     {
+	    perror("Positioning error");
+            exit(3);
+     }
+     
+     if(write(fd,init,sizeof(init)) < 0)		//writing NULL value to file
+     {
+	    perror("Writing error");
+            exit(2);
+     }
+     
+     return 0;
+}    		   
