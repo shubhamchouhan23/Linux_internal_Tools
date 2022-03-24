f2ea589403f1c90bd5646fada7224363e972e236
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-03-24 23:33:40 +0530

    Create Que_3.c

---
+++ Assignment-3/Que_3.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<sys/mman.h>
+
+void * function(void *attr)
+{
+ sleep(3);
+ printf("Thread attributes has been changed\n");
+}
+
+int main()
+{
+ pthread_attr_t attr;
+ pthread_t t1;
+ int data;
+ 
+ pthread_attr_init(&attr);
+ pthread_attr_getinheritsched(&attr, &data);
+
+ if(data==PTHREAD_INHERIT_SCHED)
+ printf("Inherit: Getting policies from parent\n");
+ else
+ printf("Explicit: Has own policies\n");
+ sleep(5);
+
+ pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+ pthread_attr_getinheritsched(&attr, &data);
+
+ if(data==PTHREAD_INHERIT_SCHED)
+ printf("Inherit: Getting policies from parent\n");
+ else
+ printf("Explicit: Has own policies\n");
+
+ pthread_create(&t1, &attr, function, NULL);
+ pthread_join(t1,NULL);
+
+ return 0;
+}
