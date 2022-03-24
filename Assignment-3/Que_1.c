37e892f05791b7f1a3b2fe52d7109ee9b3d079a6
shubhamchouhan23 <63260303+shubhamchouhan23@users.noreply.github.com>
2022-03-24 23:32:37 +0530

    Create Que_1.c

---
+++ Assignment-3/Que_1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+int a = 4;
+
+void f1(void)
+{
+ //a*a;
+ printf("Square is: %d\n",a*a);
+}
+
+void f2(void)
+{
+ //a*a*a;
+ printf("Cube is: %d\n",a*a*a);
+}
+
+void f3(void)
+{
+ a++;
+ printf("Value incremented to: %d\n",a);
+}
+
+int main()
+{
+ printf("Value: %d\n", a);
+ pthread_t t1, t2, t3;
+ 
+ pthread_create(&t1, NULL, f1, NULL);
+ pthread_create(&t2, NULL, f2, NULL);
+ pthread_create(&t3, NULL, f3, NULL);
+
+ pthread_join(t1, NULL);
+ pthread_join(t2, NULL);
+ pthread_join(t3, NULL);
+
+ printf("Shared Varible is: %d\n", a);
+ return 0;
+}
